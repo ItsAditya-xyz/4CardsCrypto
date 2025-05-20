d95057c3d86608f0180fd61b7a96858daa8558ab
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2025-05-21 02:40:23 +0530

    added menu bars

--- app/game/[id]/component/GameView.jsx
+++ app/game/[id]/component/GameView.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRef } from "react";
 import Toast from "@/components/toast";
+import Header from "@/components/header";
 
 export default function GameView({
   gameRoom,
@@ -169,13 +170,16 @@ export default function GameView({
     );
   };
 
-  if (isMobile) {
-    const all = getRelativePlayers();
-    const clockwiseOrder = [all[2], all[3], all[1], all[0]];
+ if (isMobile) {
+  const all = getRelativePlayers();
+  const clockwiseOrder = [all[2], all[3], all[1], all[0]];
 
-    return (
-      <div className='w-full h-screen overflow-y-hidden grid grid-cols-2 justify-items-center'>
+  return (
+    <div className="bg-[#0b1e2e]/80">
+      <Header />
+      <div className='w-full h-screen overflow-y-hidden grid grid-cols-2 justify-items-center pt-12'>
         {toastMsg && <Toast message={toastMsg} />}
+
         {clockwiseOrder.map((player, pos) => {
           const globalIndex = gameRoom.players.findIndex(
             (p) => p.id === player.user_id
@@ -185,6 +189,108 @@ export default function GameView({
             globalIndex;
           const isMe = player.user_id === user.id;
           const playerInfo = getPlayerInfo(player.user_id);
+
+          const isLastReceiver =
+            gameRoom.players[gameRoom.game_state?.last_receiver_index]?.id ===
+            player.user_id;
+
+          let cards;
+          if (gameRoom.status === "completed" || isMe) {
+            cards =
+              isMe && optimisticHand.length > 0 ? optimisticHand : player.hand;
+          } else if (isLastReceiver) {
+            // Show one revealed card for the last receiver
+            let injected = false;
+            cards = player.hand.map(() => {
+              if (!injected) {
+                injected = true;
+                return gameRoom.game_state?.last_passed_card;
+              }
+              return null;
+            });
+          } else {
+            cards = player.hand.map(() => null); // Hidden
+          }
+
+          const isTopRow = pos < 2;
+
+          return (
+            <div
+              key={player.user_id}
+              className='w-full  p-3 shadow-md flex flex-col items-center relative transition-transform duration-300'
+            >
+              {!isTopRow && (
+                <div className='flex flex-row space-x-1 items-center mb-2'>
+                  <Image
+                    src={playerInfo?.avatar_url || "/default-pfp.png"}
+                    alt='avatar'
+                    width={40}
+                    height={40}
+                    className='rounded-full border border-white/30'
+                  />
+                  <p className='text-sm text-white mt-1 truncate text-center max-w-[100px]'>
+                    @{playerInfo?.user_name || "player"}
+                  </p>
+                </div>
+              )}
+
+              <div className='grid grid-cols-2 gap-2'>
+                {cards.map((card, i) =>
+                  renderCard(
+                    card,
+                    i,
+                    isCurrentTurn,
+                    isMe && isMyTurn,
+                    winnerId === player.user_id
+                  )
+                )}
+              </div>
+
+              {isTopRow && (
+                <div className='flex flex-row space-x-1 items-center mt-2'>
+                  <Image
+                    src={playerInfo?.avatar_url || "/default-pfp.png"}
+                    alt='avatar'
+                    width={40}
+                    height={40}
+                    className='rounded-full border border-white/30'
+                  />
+                  <p className='text-sm text-white mt-1 truncate text-center max-w-[100px]'>
+                    @{playerInfo?.user_name || "player"}
+                  </p>
+                </div>
+              )}
+
+              {winnerId === player.user_id && (
+                <p className='text-xs text-yellow-400 font-bold animate-bounce my-1'>
+                  🏆 Winner!
+                </p>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+
+  return (
+    <div className='relative w-full  flex items-center justify-center '>
+      <Header />
+      {toastMsg && <Toast message={toastMsg} />}
+      <div className='my-10 h-[90vh]'>
+        {getRelativePlayers().map((player, pos) => {
+          const globalIndex = gameRoom.players.findIndex(
+            (p) => p.id === player.user_id
+          );
+          const isCurrentTurn =
+            (optimisticTurnIndex ?? gameRoom.game_state?.turn_index) ===
+            globalIndex;
+          const isMe = player.user_id === user.id;
+          const playerInfo = getPlayerInfo(player.user_id);
+          // const actualHand =
+          //   playerStates.find((p) => p.user_id === player.user_id)?.hand || [];
           let cards;
 
           const isLastReceiver =
@@ -209,147 +315,71 @@ export default function GameView({
             cards = player.hand.map(() => null);
           }
 
+          const layoutStyle =
+            pos === 0
+              ? "absolute bottom-0 left-1/2 -translate-x-1/2"
+              : pos === 1
+              ? "absolute left-0 top-1/2 -translate-y-1/2"
+              : pos === 2
+              ? "absolute top-0 left-1/2 -translate-x-1/2"
+              : "absolute right-0 top-1/2 -translate-y-1/2";
+
           return (
-            <div
-              key={player.user_id}
-              className='w-full bg-[#0b1e2e]/80 p-3 shadow-md flex flex-col items-center relative transition-transform duration-300'>
-              <div className='flex flex-row space-x-1 items-center mb-2'>
-                <Image
-                  src={playerInfo?.avatar_url || "/default-pfp.png"}
-                  alt='avatar'
-                  width={40}
-                  height={40}
-                  className='rounded-full border border-white/30'
-                />
-                <p className='text-sm text-white mt-1 truncate text-center max-w-[100px]'>
-                  @{playerInfo?.user_name || "player"}
-                </p>
-              </div>
-              {winnerId === player.user_id && (
-                <p className='text-xs text-yellow-400 font-bold animate-bounce my-1'>
-                  🏆 Winner!
-                </p>
-              )}
-              <div className='grid grid-cols-2 gap-2'>
-                {cards.map((card, i) =>
-                  renderCard(
-                    card,
-                    i,
-                    isCurrentTurn,
-                    isMe && isMyTurn,
+            <div key={player.user_id} className={`${layoutStyle}`}>
+              <div
+                className={`rounded-2xl transition-all duration-300 ${
+                  isCurrentTurn ? "ring-animation" : ""
+                }`}>
+                <div
+                  className={`bg-[#0b1e2e]/80 p-4 rounded-xl shadow-md transition-all duration-300 ${
                     winnerId === player.user_id
-                  )
-                )}
+                      ? "ring-2 ring-yellow-400 shadow-yellow-300"
+                      : ""
+                  }`}>
+                  {gameRoom?.game_state?.last_passed_card &&
+                    player.user_id ===
+                      gameRoom.players[gameRoom.game_state?.last_receiver_index]
+                        ?.id && (
+                      <div className='mb-2 text-yellow-300 text-sm text-center animate-ping-slow'></div>
+                    )}
+                  <div className='flex flex-row justify-center items-center space-x-1 mb-2'>
+                    <Image
+                      src={playerInfo?.avatar_url || "/default-pfp.png"}
+                      alt='avatar'
+                      width={40}
+                      height={40}
+                      className='rounded-full border border-white/30'
+                    />
+                    <p className='text-sm text-white mt-1 truncate text-center max-w-[100px]'>
+                      @{playerInfo?.user_name || "player"}
+                    </p>
+                  </div>
+                  {winnerId === player.user_id && (
+                    <p className='text-xs text-yellow-400 font-bold animate-bounce text-center my-1'>
+                      🏆 Winner!
+                    </p>
+                  )}
+
+                  <div
+                    className={`flex ${
+                      pos === 0 ? "flex-row" : "flex-wrap justify-center"
+                    } gap-2`}>
+                    {cards.map((card, i) =>
+                      renderCard(
+                        card,
+                        i,
+                        isCurrentTurn,
+                        isMe && isMyTurn,
+                        winnerId === player.user_id // new param: isWinner
+                      )
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           );
         })}
       </div>
-    );
-  }
-
-  return (
-    <div className='relative w-full h-[90vh] flex items-center justify-center mt-10'>
-      {toastMsg && <Toast message={toastMsg} />}
-      {getRelativePlayers().map((player, pos) => {
-        const globalIndex = gameRoom.players.findIndex(
-          (p) => p.id === player.user_id
-        );
-        const isCurrentTurn =
-          (optimisticTurnIndex ?? gameRoom.game_state?.turn_index) ===
-          globalIndex;
-        const isMe = player.user_id === user.id;
-        const playerInfo = getPlayerInfo(player.user_id);
-        // const actualHand =
-        //   playerStates.find((p) => p.user_id === player.user_id)?.hand || [];
-        let cards;
-
-        const isLastReceiver =
-          gameRoom.players[gameRoom.game_state?.last_receiver_index]?.id ===
-          player.user_id;
-
-        if (gameRoom.status === "completed" || isMe) {
-          cards =
-            isMe && optimisticHand.length > 0 ? optimisticHand : player.hand;
-        } else if (isLastReceiver) {
-          // Show one revealed card for the last receiver
-          let injected = false;
-          cards = player.hand.map(() => {
-            if (!injected) {
-              injected = true;
-              return gameRoom.game_state?.last_passed_card;
-            }
-            return null;
-          });
-        } else {
-          // Hide everything for others
-          cards = player.hand.map(() => null);
-        }
-
-        const layoutStyle =
-          pos === 0
-            ? "absolute bottom-0 left-1/2 -translate-x-1/2"
-            : pos === 1
-            ? "absolute left-0 top-1/2 -translate-y-1/2"
-            : pos === 2
-            ? "absolute top-0 left-1/2 -translate-x-1/2"
-            : "absolute right-0 top-1/2 -translate-y-1/2";
-
-        return (
-          <div key={player.user_id} className={`${layoutStyle}`}>
-            <div
-              className={`rounded-2xl transition-all duration-300 ${
-                isCurrentTurn ? "ring-animation" : ""
-              }`}>
-              <div
-                className={`bg-[#0b1e2e]/80 p-4 rounded-xl shadow-md transition-all duration-300 ${
-                  winnerId === player.user_id
-                    ? "ring-2 ring-yellow-400 shadow-yellow-300"
-                    : ""
-                }`}>
-                {gameRoom?.game_state?.last_passed_card &&
-                  player.user_id ===
-                    gameRoom.players[gameRoom.game_state?.last_receiver_index]
-                      ?.id && (
-                    <div className='mb-2 text-yellow-300 text-sm text-center animate-ping-slow'></div>
-                  )}
-                <div className='flex flex-row justify-center items-center space-x-1 mb-2'>
-                  <Image
-                    src={playerInfo?.avatar_url || "/default-pfp.png"}
-                    alt='avatar'
-                    width={40}
-                    height={40}
-                    className='rounded-full border border-white/30'
-                  />
-                  <p className='text-sm text-white mt-1 truncate text-center max-w-[100px]'>
-                    @{playerInfo?.user_name || "player"}
-                  </p>
-                </div>
-                {winnerId === player.user_id && (
-                  <p className='text-xs text-yellow-400 font-bold animate-bounce text-center my-1'>
-                    🏆 Winner!
-                  </p>
-                )}
-
-                <div
-                  className={`flex ${
-                    pos === 0 ? "flex-row" : "flex-wrap justify-center"
-                  } gap-2`}>
-                  {cards.map((card, i) =>
-                    renderCard(
-                      card,
-                      i,
-                      isCurrentTurn,
-                      isMe && isMyTurn,
-                      winnerId === player.user_id // new param: isWinner
-                    )
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
     </div>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,20 +1,38 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import supabase from "@/lib/supabaseClient";
 
+import modalBg from "../public/assets/modalBg.png";
+
 export default function Header({ showLogo = false }) {
+  const modalRef = useRef();
   const [user, setUser] = useState(null);
-  const [open, setOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setOpenModal(false);
+      }
+    };
+
+    if (openModal) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [openModal]);
 
   useEffect(() => {
     const fetchUser = async () => {
       const { data } = await supabase.auth.getUser();
-      console.log("user:", data?.user);
       setUser(data?.user || null);
     };
     fetchUser();
@@ -24,62 +42,90 @@ export default function Header({ showLogo = false }) {
     await supabase.auth.signOut();
     setUser(null);
     router.refresh();
+    setOpenModal(false);
   };
 
   return (
-    <div className="mt-2">
-
-  
+    <>
       {showLogo && (
-        <Link
-          href="/"
-          className="absolute top-4 left-4 z-50 block"
-        >
+        <Link href='/' className='absolute top-4 left-4 z-50 block'>
           <Image
-            src="/assets/logo.png"
-            alt="Logo"
+            src='/assets/logo.png'
+            alt='Logo'
             width={100}
             height={100}
-            className="hover:opacity-90 transition"
+            className='hover:opacity-90 transition'
           />
         </Link>
       )}
 
       {user && (
-        <div className="absolute top-4 right-4 z-50">
-          <div className="relative">
-            <button
-              onClick={() => setOpen(!open)}
-              className="flex items-center gap-2"
-            >
-              <p className="font-medium text-gray-100 mb-2">
-                GM, {user.user_metadata?.user_name || "Player"}
-              </p>
-              <Image
-                src={
-                  user.user_metadata?.avatar_url ||
-                  "/assets/default-avatar.png"
-                }
-                alt="Avatar"
-                width={40}
-                height={40}
-                className="rounded-full border-2 border-white hover:opacity-90 transition"
-              />
-            </button>
-
-            {open && (
-              <div className="absolute right-0 mt-2 bg-white border rounded-md shadow-md p-3 text-sm min-w-[160px]">
-                <button
-                  onClick={handleLogout}
-                  className="text-red-500 hover:underline"
-                >
-                  Log out
-                </button>
-              </div>
-            )}
-          </div>
+        <div className='absolute top-4 right-4 z-50'>
+          <button
+            onClick={() => setOpenModal(true)}
+            className='flex items-center gap-2'>
+            <p className='font-medium text-gray-100 mb-2'>
+              GM, {user.user_metadata?.user_name || "Player"}
+            </p>
+            <Image
+              src={
+                user.user_metadata?.avatar_url || "/assets/default-avatar.png"
+              }
+              alt='Avatar'
+              width={40}
+              height={40}
+              className='rounded-full border-2 border-white hover:opacity-90 transition'
+            />
+          </button>
         </div>
       )}
+
+     {openModal && (
+  <div className="fixed inset-0 z-50 bg-black/60 backdrop-blur-sm flex items-center justify-center">
+    <div ref={modalRef} className="relative w-[380px] h-[480px] sm:w-[420px] sm:h-[500px]">
+      <Image
+        src={modalBg}
+        alt="Modal Background"
+        fill
+        className="absolute inset-0 object-contain z-[-1] pointer-events-none"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-black text-center space-y-5 px-6 pt-12">
+      
+
+        <Link
+          href="/"
+          onClick={() => setOpenModal(false)}
+          className="hover:underline font-bold"
+        >
+          Home
+        </Link>
+        <Link
+          href="/profile"
+          onClick={() => setOpenModal(false)}
+          className="hover:underline font-bold"
+        >
+          Profile
+        </Link>
+        <Link
+          href="/leaderboard"
+          onClick={() => setOpenModal(false)}
+          className="hover:underline font-bold"
+        >
+          Leaderboards
+        </Link>
+
+        <button
+          onClick={handleLogout}
+          className="text-red-600 hover:text-red-700 font-semibold"
+        >
+          Log out
+        </button>
+
+       
+      </div>
     </div>
+  </div>
+)}
+    </>
   );
 }
